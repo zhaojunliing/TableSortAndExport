dfdebbe32302d489c53f3d60596602173e9c7b51
bigablecat <bigablecat@users.noreply.github.com>
2017-07-18 14:57:59 +0800

    #2nd commit

--- js/index.js
+++ js/index.js
@@ -1,7 +1,20 @@
+/*
+ * 表格导出插件tableExport下载地址 
+ * https://github.com/hhurz/tableExport.jquery.plugin
+ * 
+ * 表格排序插件tableSorter下载地址
+ * https://github.com/Mottie/tablesorter
+ * 
+ * */
+
+/**
+ * 页面初始化
+ */
 jQuery(document).ready(function() {
 	initTableSorter(); //初始化表格排序
+	$('tbody').html("");//重新加载时清空原有数据
 	loadData(); //加载数据
-	$('#export').on("click",function(){
+	$('#export').on("click",function(){ //定义点击事件
 		exportData();//导出数据
 	});
 });
@@ -10,6 +23,7 @@ jQuery(document).ready(function() {
  * 加载数据
  */
 function loadData() {
+	//mockData来自dataset.js定义的模拟数据
 	fillTable(mockData); //用模拟数据填充表格
 }
 
@@ -22,7 +36,7 @@ function loadDataFromServer() {
 		dataType: "json",
 		url: 'posturl', //服务请求地址 
 		success: function(data) {
-			fillTable(data); //数据填充表格
+			fillTable(data); //用服务器获取的数据填充表格
 		}
 	});
 }
@@ -34,20 +48,19 @@ function loadDataFromServer() {
  * @returns
  */
 function fillTable(data) {
-	var html = new Array();
-	for(var i = 0; i < data.length; i++) {
-		html.push('<tr class="even" id=\'' + data[i].id + '\'>');
-		html.push(
-			'<td scope="row">' + i + '</td>');
-		html.push('<td>' + data[i].name + '</td>');
-		html.push('<td>' + data[i].author + '</td>');
-		html.push('<td>' + data[i].pubdate + '</td>');
-		html.push('<td>' + data[i].pagesum + '</td>');
+	var html = new Array(); //新建数组
+	for(var i = 0; i < data.length; i++) { //遍历数据
+		html.push('<tr id=\'' + data[i].id + '\'>');
+		html.push('<td scope="row">' + i + '</td>');  //序号
+		html.push('<td>' + data[i].name + '</td>');   //书名
+		html.push('<td>' + data[i].author + '</td>'); //作者
+		html.push('<td>' + data[i].pubdate + '</td>');//出版日期
+		html.push('<td>' + data[i].pagesum + '</td>');//总页数
 		html.push('</tr>');
 	}
-	$('tbody').append(html.join(''));
+	$('tbody').append(html.join('')); //将数据拼接后附加到当前表格
 	updateTableSorter(); //动态添加数据后更新排序数据和样式
-	addThIndex(); //为表单添加序号
+	addRowNum(); //为表格重新添加序号,让序号仍然按升序排列
 }
 
 /**
@@ -57,9 +70,10 @@ function fillTable(data) {
  */
 function initTableSorter(){
 	$("table").tablesorter({
+//		widgets: ['zebra'], //表格斑马纹
 		'headers': { 0:{sorter: false}}
 	}).bind("sortEnd",function() { //排序完成后绑定事件
-		addThIndex();
+		addRowNum();//为表格添加行数
 	});
 }
 
@@ -79,9 +93,12 @@ function updateTableSorter(){
  * 
  * @returns
  */
-function addThIndex() {
-	$('td[scope="row"]').each(function() {
-		$(this).text($(this).parent().index() + 1);
+function addRowNum(){
+	$('tr').each(function(){
+		//行数为当前<tr>元素相对于父元素的索引+1
+		var rowNum = $(this).index()+1; 
+		//将行数填入每行第一列
+		$(this).find('td').eq(0).text(rowNum); 
 	});
 }
 
@@ -91,6 +108,9 @@ function addThIndex() {
  */
 function exportData(){
 	var tableNameStr = "book-info"; //定义文件名
+	//第一个参数定义输出的文件名
+	//第二个参数定义了表格元素的选择器
+	//第三个参数定义了输出格式,此处是cvs格式
 	doExport(tableNameStr, 'table',{type: 'csv'});
 }
 
@@ -106,6 +126,10 @@ function doExport(tableNameStr, selector, params) {
     tableName: tableNameStr,
     worksheetName: tableNameStr
   };
-  $.extend(true, options, params);
+  //$.extend第一个参数为true,执行深度合并
+  //如果options的属性是对象或数组,则递归地将params属性中的对象或数组添加到options
+  //$.extend具体用法参照官网http://api.jquery.com/jQuery.extend/
+  $.extend(true,options, params);
+  //根据参数设定执行表格导出
   $(selector).tableExport(options);
 }
